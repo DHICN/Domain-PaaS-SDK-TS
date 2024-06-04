faf1a0a7704f11f4b3c5361011281d24bb848a82
wangxingguo <theplayer_123@foxmail.com>
2024-06-04 09:33:22 +0800

    <<v2.7.6>> Feat:update wwtp infrastructure api v2.7.6

--- src/wwtp-domain-infrastructure-service/models/alarm-config-in-out.ts
+++ src/wwtp-domain-infrastructure-service/models/alarm-config-in-out.ts
@@ -80,16 +80,16 @@ export interface AlarmConfigInOut {
 }
 
 export const AlarmConfigInOutAlarmTypeEnum = {
-  NUMBER_0: 0,
-  NUMBER_1: 1,
-  NUMBER_2: 2,
+  '0': 0 as number,
+  '1': 1 as number,
+  '2': 2 as number,
 } as const
 
 export type AlarmConfigInOutAlarmTypeEnum =
   (typeof AlarmConfigInOutAlarmTypeEnum)[keyof typeof AlarmConfigInOutAlarmTypeEnum]
 export const AlarmConfigInOutNodeTypeEnum = {
-  NUMBER_0: 0,
-  NUMBER_1: 1,
+  '0': 0 as number,
+  '1': 1 as number,
 } as const
 
 export type AlarmConfigInOutNodeTypeEnum =
